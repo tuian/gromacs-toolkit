ea8244036c9ad69b2a610966c40cbe99dee5771c
Jan Domanski <jandom@gmail.com>
2011-02-07 23:04:21 +0100

    added a swap function

--- conf2conf.py
+++ conf2conf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # python lipid2lipid.py <FILE> <OLD LIPID TYPE> <NEW LIPID TYPE>
 # python lipid2lipid.py bilayer.gro POPC DAPC
-# AUTHOR: Clement Arnarez
-# Improved by: Jan Domanski
+# Author: Jan Domanski
+# Gro parsing routine by: Clement Arnarez
 # Version 1.1
 import sys
 from optparse import OptionParser
@@ -91,6 +91,31 @@ def remove(molecules, argv):
             counter = 0
     
     return molecules
+
+def swap(molecules, argv):
+    """
+    conf2conf -a swap type new_type numerator denominator
+    """
+    
+    type = argv[0]
+    new_type = argv[1]
+    numerator = int(argv[2])
+    denominator = int(argv[3])
+    counter = 0
+    for k, [v, list] in molecules.items():
+        if not v == type:
+            continue
+        
+        counter += 1
+        if counter <= numerator:
+            atom_group = molecules[k][1]
+            for atom in atom_group:
+              atom["name"] = new_type
+        if counter >= denominator:
+
+            counter = 0
+    
+    return molecules
             
 def retype(molecules, argv):
     """
@@ -166,8 +191,11 @@ def main(argv):
     if operation == "remove":
         molecules = remove(molecules, args)
 
+    if operation == "swap":
+        molecules = swap(molecules, args)
+
     if operation == "type":
-	molecules = retype(molecules, args)
+	    molecules = retype(molecules, args)
         
     if operation == "dump":
         composition(molecules)
